e1d63fa50adf46d66fa25d80e20345bd96a40ca2
Thomas Fabre <thomas.fabre@outlook.fr>
2024-02-02 16:11:44 +0100

    front shop list

--- lib/components/location_list_tile.dart
+++ lib/components/location_list_tile.dart
@@ -1,42 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:yper_test/models/shop.dart';
 import 'package:yper_test/providers/location_provider.dart';
+import 'package:yper_test/theme/app_theme.dart';
 
 class LocationListTile extends StatelessWidget {
   const LocationListTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<LocationProvider>(
-        builder: (context, locationProvider, child) {
-      return ListView.builder(
-          itemCount: locationProvider.nearestYperShops.length,
-          itemBuilder: (context, index) {
-            return Column(
-              children: [
-                ListTile(
-                  title: Text(locationProvider.nearestYperShops[index].name),
-                  subtitle: Text(
-                      locationProvider.nearestYperShops[index].fullAddress),
-                ),
-                const Divider()
-              ],
-            );
-          });
-    });
-
-    // ListView.builder(
-    //     itemCount: 12,
-    //     itemBuilder: (context, index) {
-    //       return Column(
-    //         children: [
-    //           ListTile(
-    //             title: Text("Location $index"),
-    //             subtitle: Text("Adresse $index"),
-    //           ),
-    //           const Divider()
-    //         ],
-    //       );
-    //     });
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Consumer<LocationProvider>(
+          builder: (context, locationProvider, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            locationProvider.nearestYperShops.isNotEmpty
+                ? const Text("LE PLUS PROCHE",
+                    style: TextStyle(
+                      letterSpacing: 1.5,
+                      color: AppTheme.mainColor,
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                    ))
+                : const SizedBox(),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: locationProvider.nearestYperShops.length,
+                  itemBuilder: (context, index) {
+                    Shop shop = locationProvider.nearestYperShops[index];
+                    return Column(
+                      children: [
+                        ListTile(
+                          minVerticalPadding: 0,
+                          contentPadding: const EdgeInsets.all(0),
+                          title: Text(
+                            shop.name,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          subtitle: Text(
+                            shop.fullAddress,
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        const Divider()
+                      ],
+                    );
+                  }),
+            ),
+          ],
+        );
+      }),
+    );
   }
 }
